409ee23e600cfa5a6be909cde2eb724f10fd7db0
mlgmdd <109269794+mlgmdd@users.noreply.github.com>
2023-12-19 19:03:16 +0800

    Bug Fixed

--- main.py
+++ main.py
@@ -2,18 +2,6 @@ from app import App
 
 if __name__ == "__main__":
     app = App()
-
-    # app.local_data.clear_group_msg(group_id=603161290)
-    msgs = app.bot.get_group_msg(group_id=732649746)
-    app.local_data.save_group_msg(msgs, group_id=732649746)
     app.main_loop()
-
-
-
-    # r = app.get_summary(group_id=603161290)
-    # print(r)
-
-
-    # print(msgs)
 
 
